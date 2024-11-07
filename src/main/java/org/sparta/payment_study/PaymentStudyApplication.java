e6b3a30afd3139c7f30d7f49a3025b25b00e56af
coding-suk <hjldd0523@naver.com>
2024-11-07 13:59:57 +0900

    [commit]first init

---
+++ src/main/java/org/sparta/payment_study/PaymentStudyApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.payment_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/payment_study/PaymentStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.payment_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
