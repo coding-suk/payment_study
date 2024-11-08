5f5cd01e92333cbd7678b2c448b4686711430adb
coding-suk <hjldd0523@naver.com>
2024-11-08 11:16:18 +0900

    [commit] 파일이 없기 때문에 커밋 후 모든 로직들 수정

---
+++ src/main/java/org/sparta/payment_study/payment/entity/Payment.java
@@ -0,0 +1,14 @@
+//package org.sparta.payment_study.payment.entity;
+//
+//import jakarta.persistence.Entity;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor
+//public class Payment {
+//
+//    @Id
+//
+//}
